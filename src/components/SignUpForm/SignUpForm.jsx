7432ad9d425e8628639ee5f6c730fb7f648b7243
Tiffany <tiffany.lam@gmail.com>
2022-04-02 08:18:19 -0400

    updated login and signup forms

--- src/components/SignUpForm/SignUpForm.jsx
+++ src/components/SignUpForm/SignUpForm.jsx
@@ -46,8 +46,9 @@ export default class SignUpForm extends Component {
     const disable = this.state.password !== this.state.confirm;
     return (
       <div>
-        <div className="form-container">
         <h1>Behavioural Interview Questionnaire</h1>
+        <div className="form-container">
+
           <form autoComplete="off" onSubmit={this.handleSubmit}>
             <label>Name</label>
             <input type="text" name="name" value={this.state.name} onChange={this.handleChange} required />
